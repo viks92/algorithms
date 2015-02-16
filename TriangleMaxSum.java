7f5dc31603ad60f9af2453d8a84ad0f009b591d8
viks92 <cutevikhyati@gmail.com>
2015-02-15 20:11:47 -0800

    Update TriangleMaxSum.java

--- TriangleMaxSum.java
+++ TriangleMaxSum.java
@@ -3,8 +3,17 @@ import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
 /**
- * A recent programming challenge solved me. Will update soon on input and output
- * http://www.yodlecareers.com/puzzles/triangle.html 
+ * A recent programming challenge solved by me.
+By starting at the top of the triangle and moving to adjacent numbers on the row below, the maximum total from top to bottom 
+is 27.
+
+        5
+      9  6
+    4   6  8
+  0   7  1   5
+
+I.e. 5 + 9 + 6 + 7 = 27.
+
  */  
 public class TriangleMaxSum {
     
